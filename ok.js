c8cde86a5d0279888a2b08e715441703fed0f9cc
SilentPowers <cnlakour@gmail.com>
2021-03-12 12:09:12 -0600

    Update ok.js

--- ok.js
+++ ok.js
@@ -2,25 +2,27 @@
 // function okClick()
 // {alert(" You clicked ok!! " ) };
 
-// called when page loads; sets up event handlers
-// function pageLoad() {
-//   document.getElementById("ok").onclick = okClick;
-//   }
-//   function okClick() {
-//   alert(" You clicked ok!!");
-//   }
+//called when page loads; sets up event handlers
+function pageLoad() {
+  document.getElementById("ok").onclick = okClick;
+  }
+  function okClick() {
+    const okButton = document.getElementById("ok");
+    okButton.className = "fancy";
+  alert(" You clicked ok!!");
+  }
 
 //annoymous fcn
 
-  window.onload = function(){
-    document.getElementById("ok").onclick = function(){
-      const okButton = document.getElementById("ok");
-      okButton.className = "fancy";
-      alert("You clicked ok!");
+  // window.onload = function(){
+  //   document.getElementById("ok").onclick = function(){
+  //     const okButton = document.getElementById("ok");
+  //     okButton.className = "fancy";
+  //     alert("You clicked ok!");
 
-    }
-  }
+  //   }
+  // }
   
-  //pageLoad; // onload is a browser event
+  window.onload = pageLoad; // onload is a browser event
 
 
