a72826e62b48a90a44677357759d552ef03ff783
SilentPowers <cnlakour@gmail.com>
2021-03-24 15:01:04 -0500

    labs

--- labsW1D3/app.js
+++ labsW1D3/app.js
@@ -1,5 +1,17 @@
 "use strict";
 
+/*
+Write a constructor function Accumulator(initialValue, increment). The object it creates should:
+• Store the current accumulated value in a property currentValue. The constructor should set this
+to be initialValue.
+• The accumulate method should increase the currentValue by the increment.
+• The report method should return the currentValue.
+• Run this with the accumulatorTests.js Mocha test file.
+Be sure to include the link to your <username>.github.io/cs303 home page and that the Mocha tests for
+this assignment show up for the W1D3 link.
+*/
+
+
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
 module.exports = { Accumulator, Calculator }; //add all of your function names here that you need for the node mocha tests
 
